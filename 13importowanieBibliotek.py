320c4f2315fc1af3d1ff949dc591a25d88118214
Kuba <j.rafal.wroblewski@gmail.com>
2020-07-01 23:17:06 +0200

    update

--- 13importowanieBibliotek.py
+++ 13importowanieBibliotek.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
-import math
+import math     # importujemy całą bibioteke "math" aby wywołać funkcje z niej trza zacząć od "math. ..."
 
+print(math.gcd(45, 55))
 print(math.ceil(4.2))
 print(math.ceil(1.45))
 
-from math import fabs
+help(math.ceil)
 
+from math import fabs   # zaimportowalismy całą biblioteke "math" tu możemy odrazu podawać funkcje "fabs..."
 
 print(fabs(1))
 
-print(math.gcd(45,55))
+help(fabs)  # podpowiada nam co robi dana funckja :)
+
+help(tuple)
+
+# Built-in Functions # wbudowane funkcje w python około 70
--- 14UnikanieBledowZwywolaniemFunkcji.py
+++ 14UnikanieBledowZwywolaniemFunkcji.py
@@ -2,6 +2,12 @@
 
 import math
 
+imieMale = "kuba"
+imieDuze = "KuBA"
+print(imieMale.capitalize())
+print(imieDuze.capitalize())
+print(imieDuze.upper())
+
 imie = "Kuba"
 imiePoZmianie = imie.lower()    # 2 część kodu "eimie.lower()" zostaje całkowicie zamienione na "kuba"
 
@@ -9,3 +15,8 @@ print(imiePoZmianie)
 
 print(math.sqrt(4))     # tak jak i tu cały wyraz jest zamieniany na samą cyfre "2.0"
 
+imie = "Stefan"
+nazwisko = "Kochanowski"
+
+print(imie.lower())
+print(nazwisko.lower())
