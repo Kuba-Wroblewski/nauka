6f16858bfa590544533ce330682e3812cb76ada9
Kuba <j.rafal.wroblewski@gmail.com>
2020-10-10 15:52:32 +0200

    update 10.10.2020

--- 47-49CwiczenieProgram.LiczacyPowierzchniE.py
+++ 47-49CwiczenieProgram.LiczacyPowierzchniE.py
@@ -44,3 +44,14 @@ while True:
 
     else:
         print("Podałeś błędny parametr z zakresu")
+
+# from enum import IntEnum
+# Menu_Dni_Tygodnia = IntEnum("Menu_Dni_Tygodnia", {"Poniedziałek":20, "Inny_dzien":1})
+# print(list(Menu_Dni_Tygodnia))
+# wybor = int(input("""\nJaki dziś dzień ?:
+#     1. Poniedziałek
+#     2. Inny dzień\n"""))
+# if wybor == Menu_Dni_Tygodnia.Poniedziałek:
+#     print("Może i poniedziałek")
+# if wybor == Menu_Dni_Tygodnia.Inny_dzien:
+#     print("Inny dzień")
