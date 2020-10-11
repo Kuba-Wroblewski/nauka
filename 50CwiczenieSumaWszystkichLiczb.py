204cfc9dd56980e9bae01ced464e2fa4701307dc
Kuba <j.rafal.wroblewski@gmail.com>
2020-10-11 08:09:39 +0200

    update 11.10.2020

--- 50CwiczenieSumaWszystkichLiczb.py
+++ 50CwiczenieSumaWszystkichLiczb.py
@@ -9,11 +9,48 @@ zwroci
 15
 """
 
-wynik = []
-wybor = int(input("\nWprowadz liczbe do której bedziemy sumować liczby od 1\n"))
-for element in range(1, wybor + 1):
-    wynik.append(element)
-print(sum(wynik))
+def sumuj_do():
+    wynik = 0
+    wybor = int(input("\nWprowadz liczbe do której bedziemy sumować liczby od 1\n"))
+    for element in range(1, wybor + 1):
+        wynik = wynik + element
+    return wynik
 
+def sumuj_do1():
+    wynik = []
+    wybor = int(input("\nWprowadz liczbe do której bedziemy sumować liczby od 1\n"))
+    for element in range(1, wybor + 1):
+        wynik.append(element)
+    return sum(wynik)
 
+def sumuj_do2():
+    wybor = int(input("\nWprowadz liczbe do której bedziemy sumować liczby od 1\n"))
+    return sum([liczba for liczba in range(1, wybor +1)])
 
+def sumuj_do3():
+    wybor = int(input("\nWprowadz liczbe do której bedziemy sumować liczby od 1\n"))
+    return sum({liczba for liczba in range(1, wybor +1)})
+
+def sumuj_do4():
+    wybor = int(input("\nWprowadz liczbe do której bedziemy sumować liczby od 1\n"))
+    return sum((liczba for liczba in range(1, wybor +1)))
+
+def sumuj_do5():
+    wybor = int(input("\nWprowadz liczbe do której bedziemy sumować liczby od 1\n"))
+    return ((1 + wybor) / 2 * wybor)
+
+"""
+wzór na ciąg arytmetyczny w matematyce
+(1+liczba) / 2 * liczba
+13 * 25
+10* 25 = 250
+3 * 25 = 75
+250 + 75 = 325
+"""
+
+# print(sumuj_do())
+# print(sumuj_do1())
+# print(sumuj_do2())
+# print(sumuj_do3())
+# print(sumuj_do4())
+# print(sumuj_do5())
