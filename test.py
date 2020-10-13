21f48819d6f0e33ceee814839128292093305bf0
Kuba <j.rafal.wroblewski@gmail.com>
2020-10-13 22:36:26 +0200

    update 11.10.2020

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ import time
 # napisz funkcje która będzie sprawdzała czy jakis element znajduje sie w kontenerze.
 # jesli sie znajduje funkcja ma zwrócic tak jak sie nie znajduje to ma wyswietlic nie i sprawdz co jest szybsze.
 
-
 def function_performance(func, how_many_times=1, **arg):
     suma = 0
     print(arg.get("liczba_szukana"))
