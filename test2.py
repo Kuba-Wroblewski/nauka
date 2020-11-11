44f885678f97afc2335408dca8ad722ef91f16e5
Kuba <j.rafal.wroblewski@gmail.com>
2020-11-12 00:25:12 +0100

    update

--- test2.py
+++ test2.py
@@ -3,3 +3,21 @@
 import random
 
 
+
+def will_weapon_hit(weaponChanceToHitPercentage):
+    isHitChance = random.uniform(0,100)
+    if (isHitChance < weaponChanceToHitPercentage):
+        return "hit"
+    else:
+        return "not hit"
+
+print(will_weapon_hit(50))
+
+x = 0
+listHit = []
+while x < 100:
+    x = x + 1
+    listHit.append(will_weapon_hit(50))
+
+print(listHit)
+print(listHit.count("hit"))
