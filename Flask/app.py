0e8842d4f6a1323470f68a8f2d4f02a5009ecd6a
Kuba <j.rafal.wroblewski@gmail.com>
2020-11-23 15:12:29 +0100

    update

--- Flask/app.py
+++ Flask/app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'autor': 'Jakub Wróblewski',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 23, 2020'
+    },
+    {
+        'autor': 'Jane Porn',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'April 24, 2020'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Hello, World!</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>next page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
