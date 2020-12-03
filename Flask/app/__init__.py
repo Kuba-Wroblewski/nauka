5839f6182a65304e1dff653d9cf1b6049553f05b
Kuba <j.rafal.wroblewski@gmail.com>
2020-12-03 15:16:54 +0100

    update

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fc0ed4935868eeba22b8244b9eadfb57'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from app import routes
