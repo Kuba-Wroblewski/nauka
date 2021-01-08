9ff4af9a238dc038b83a0c717455a5d60292ec59
Kuba <j.rafal.wroblewski@gmail.com>
2021-01-08 16:56:09 +0100

    update 16:55 8.01.2021

--- testowanie.py
+++ testowanie.py
@@ -2,38 +2,38 @@
 
 import random
 from enum import Enum
-
-Zdarzenie = Enum('Event',['Chest', 'Empty'])
-
+Zdarzenie = Enum('Event',['Skrzynia', 'Nic'])
 słownikZdarzeń = {Zdarzenie.Skrzynia: 0.6, Zdarzenie.Nic: 0.4}
-listaZdarzeń = list(listaZdarzeń.keys())
-prawdopodobneZdarzenia = list(listaZdarzeń.values())
-
+listaZdarzeń = list(słownikZdarzeń.keys())
+prawdopodobneZdarzenia = list(słownikZdarzeń.values())
 print("Witaj w mojej grze <LABIRYNT>, Masz tylko 5 ruchów, możesz zdobyć pewną nagrode, lub nie")
-
 Kolor = Enum('Kolory', {'Green': 'zielony', 'Orange': 'pomarańczowy', 'Purple': 'fioletowy', 'Gold': 'złoty'})
-  
-chestColoursDictionary = {Kolor.Green :  0.75, Kolor.Orange : 0.2, Kolor.Purple : 0.04, Kolor.Gold : 0.01}
-
-skrzyniKolorLista = tuple(skrzyniKolorLista.keys())
-skrzyniKolorPrawdopodobieństwo = tuple(skrzyniKolorPrawdopodobieństwo.values())
-
-rewardsForChests = {skrzyniKolorLista[reward]: (reward + 1) * (reward + 1) * 1000
-                    for reward in range(len(skrzyniKolorLista))}
+skrzyniKolorSłownik = {Kolor.Green :  0.75, Kolor.Orange : 0.2, Kolor.Purple : 0.04, Kolor.Gold : 0.01}
+skrzyniKolorLista = tuple(skrzyniKolorSłownik.keys())
+skrzyniKolorPrawdopodobieństwo = tuple(skrzyniKolorSłownik.values())
+nagrodaZaSkrzynki = {skrzyniKolorLista[reward]: (reward + 1) * (reward + 1) * 1000
+                    for reward in range(len(skrzyniKolorSłownik))}
 
 import random
-
-długość_naszej_gry = 5
+zdobyteZłoto = 0
+długość_naszej_gry = 2
 
 while długość_naszej_gry > 0:
     odpowieć_gracza = input("Czy chcesz iść do przodu ?")
     if (odpowieć_gracza == "tak"):
         print("Wspaniale, zobaczmy co zyskałeś...")
-        random.choices
+        WylosowanyZdarzenie = random.choices(listaZdarzeń, prawdopodobneZdarzenia)[0]
+        if (WylosowanyZdarzenie == Zdarzenie.Skrzynia):
+            print("Wylosowałeś skrzynke")
+            WylosowanaSkrzynia = random.choices(skrzyniKolorLista, skrzyniKolorPrawdopodobieństwo)[0]
+            print("Kolor skrzyni jest:", WylosowanaSkrzynia)
+            nagrodaGracza = nagrodaZaSkrzynki[WylosowanaSkrzynia]
+            zdobyteZłoto += nagrodaGracza
+        elif (listaZdarzeń == Zdarzenie.Nic):
+            print("Nic nie wylosowałeś masz pecha")
     else:
-        print("Możesz iść tylko do przodu MEAN 'tak'")
+        print("Możesz iść tylko do przodu im sorry")
         continue
 
     długość_naszej_gry -= 1
-
-
+print("Gratulacje zdobyłes", zdobyteZłoto)
