5ffdb0c2299efe77ff4e8518725dadaf27741a68
Kuba <j.rafal.wroblewski@gmail.com>
2021-02-02 16:37:35 +0100

    update

--- 77-78CzymJestJSON.py
+++ 77-78CzymJestJSON.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
 
 """
-JSON
+
 JSON - to format danych, który posiada wspólne reguły zapisu danych dla wszystkich języków programowania.
 Dzięki czemu dane raz zapisane w formacie JSON mogą być odczytane przez każdego programistę nawet jeśli programuje w innym języku programowania.
 
+json.dumps - zapisuje dane do postaci Stringowej json
+json.dump - zapisuje dane do pliku w postaci json
+ensure_ascii=False (or True) - False pokazuje polskie znaki w pliku json True (domyślnie) nie pokazuje
 
 """
 
 import json
 
 imiona = ["""
-Arkadiusz Włodarczyk
-Meadow
-Carla Baker
-Lily-May Rosas
-Inez Emerson
-Stuart Harrington
-Lemar Calderon
-Zahraa Joyner
-Niam Martin
-Ezmae Hull
-Athena Frost
-ArkadiuszMarx"""
-]
+Arkadiusz Włodarczyk,
+Meadow,
+Carla Baker,
+Lily-May Rosas,
+Inez Emerson,
+Stuart Harrington,
+Lemar Calderon,
+Zahraa Joyner,
+Niam Martin,
+Ezmae Hull,
+Athena Frost,
+Arkadiusz Marx"""]
 
-print(json.dumps(imiona))
+encodeImiona = json.dumps(imiona)
+print(encodeImiona)
+# Zapisuje do JSON  w postaci stringowej
 
+
+with open("sample.json", "w", encoding="UTF-8") as file:
+    json.dump(imiona, file, ensure_ascii=False)
+# zapisuje dane do pliku w postaci JSON
