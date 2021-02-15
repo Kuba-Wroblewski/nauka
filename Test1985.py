2e6bf3c7abec41deb7e6839032249c6a0806e779
Kuba <j.rafal.wroblewski@gmail.com>
2021-02-15 16:22:53 +0100

    update

--- Test1985.py
+++ Test1985.py
@@ -2,6 +2,7 @@
 
 import json
 import pprint
+import requests
 
 people =  {
         "IcFDG2bO9AYDF651DoyH": {'name': 'Łukasz', 'age': 27, 'sex': 'Male'},
@@ -14,12 +15,27 @@ people =  {
         "KQ4bir3y4tlkbG69I7zq": {'name': 'Marie', 'age': 42, 'sex': 'Female'},
         "94cp4hsyZP2BnCh4D34z": {'name': 'Agness', 'age': 25, 'sex': 'Female'},
         "Vr4wRdkljeEs46Czxo54": {'name': 'Chiara', 'age': 17, 'sex': 'Male'},
-         }
+    }
 
-wynik = json.dumps(people, ensure_ascii=False)
-# print(wynik)
+listUrl = {'''
+        https://flask.palletsprojects.com/en/1.1.x/quickstart/,
+        https://docs.python.org/3/tutorial/errors.html,
+        https://pypi.org/project/requests2/,
+        https://www.google.pl/,
+        https://www.ggoooggllee.pl/,
+        '''}
 
-with open("jakis_kurwa_plik", "r") as file:
-    wynik = json.load(file)
+UrlNotFound = []
+def czeck_url(url):
 
-pprint.pprint(wynik)
+    response = requests.get(url)
+    if response.status_code == 404:
+        print("Nie ma takiej strony")
+        with open("listNotFoundUrl.txt", "w") as file:
+            file.writelines()
+    else:
+        print("A ta stronka jest")
+
+
+
+czeck_url("https://www.google.pl/")
