b81d2213a3186dcb7bdf16d19a040cc2bac6f140
Kuba <j.rafal.wroblewski@gmail.com>
2021-02-16 17:06:16 +0100

    update

--- 81CzymJestPIP.py
+++ 81CzymJestPIP.py
@@ -3,10 +3,16 @@
 import requests
 
 """
-          
-
-
-
-
+pip - pip installs package - instalator pakunków
+PyPi - Python Package index - index (spis) pakunków do Pythona
 
 """
+
+
+response = requests.get("https://flask.palletsprojects.com/en/1.1.x/quickstart/")
+response.status_code
+
+print(response)
+
+# status_code = 200 - łączy się ze stroną, strona istnieje
+# status_code = 404 - nie łączy się, nie ma takiej strony
