6952e06fd3e0e9b4a8b916e68b5a7b18b224e375
Kuba <j.rafal.wroblewski@gmail.com>
2021-02-23 14:48:36 +0100

    update

--- 82CwiczenieCzyStronaIstnieje.py
+++ 82CwiczenieCzyStronaIstnieje.py
@@ -4,26 +4,28 @@ import requests
 
 listaUrl = [ 'https://flask.palletsprojects.com/en/1.1.x/quickstart/', 'https://github.com/fuck', 'https://www.google.pl/', 'https://www.google.pl/nocos' ]
 
+
 class Mojaklassa():
 
-    def work_url(listUrl):
+    def work_url(self, listaUrl):
         list_work_url = []
-        for element in listUrl:
+        for element in listaUrl:
             response = requests.get(element)
             if response.status_code == 200:
                 list_work_url.append(element + "\n")
+                
             else:
-                # print("This page is not avaliable:", element, response)
-                page_is_not_working = element
-        print(list_work_url)
-        # return list_work_url
+                print("This page is not avaliable:", element, response)
+        return
 
 
 
-# def work_url2(list_work_url):
-    # with open("work_url.txt", "w") as file:
-        # file.writelines(list_work_url)
+def work_url2(list_work_url):
+    with open("work_url.txt", "w") as file:
+        file.writelines(list_work_url)
 
 
-# Mojaklassa(work_url)
+wynik = Mojaklassa()
+wynik.work_url(listaUrl)
+work_url2(list_work_url)
 
--- test2.py
+++ test2.py
@@ -4,6 +4,12 @@ import test_pierwszy
 
 class Calculator():
 
+    def __init__(self):
+        print("init")
+    def __del__(self):
+        print("del")
+
+
     def dodaj(self, a, b):
         wynik = a + b
         print(wynik)
@@ -12,5 +18,8 @@ class Calculator():
         print(wynik)
         
 calc = Calculator()
+calc2 = Calculator()
+
 calc.dodaj(5, 5)
+calc2.dodaj(14, 5)
 
