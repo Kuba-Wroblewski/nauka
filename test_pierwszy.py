a712ca2a5c97ac30924c2b88077a50e46148941e
Kuba <j.rafal.wroblewski@gmail.com>
2021-03-01 17:29:52 +0100

    update

--- test_pierwszy.py
+++ test_pierwszy.py
@@ -2,17 +2,17 @@
 
 
 
-# def pole_prostokąta(a, b):
-#     print(a * b)
-#     return a * b
+def pole_prostokąta(a, b):
+    print(a * b)
+    return a * b
 
 
-# poleProstokata = pole_prostokąta(2, 78)
-# print(5 * pole_prostokąta(5,50))
+poleProstokata = pole_prostokąta(2, 78)
+print(5 * pole_prostokąta(5,50))
 
-def dzielenie(a, b):
-    
-    return a / b
+# def dzielenie(a, b):
+
+#     return a / b
 
 
-print(dzielenie(10, 0))
+# print(dzielenie(10, 0))
