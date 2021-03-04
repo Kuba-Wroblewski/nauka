369519d75febaf2fbd4179000301acc012002b25
Kuba <j.rafal.wroblewski@gmail.com>
2021-03-04 16:42:57 +0100

    update

--- Test1985.py
+++ Test1985.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 
-import requests
+import requests # import Module
 
-lista_url = ['https://flask.palletsprojects.com/en/1.1.x/quickstart/', 'https://github.com/fuck',
-            'https://www.google.pl/', 'https://www.google.pl/nocos']
+sample_file_url = [
+                    'https://flask.palletsprojects.com/en/1.1.x/quickstart/', 'https://github.com/fuck',
+                    'https://www.google.pl/', 'https://www.google.pl/nocos'
+                   ]
 
-my_work_url = [] # Variable 
-def check_url(urla):  # Method
-    for url in urla:
-        response = requests.get(url)
-        if response.status_code == 200: # Function
+my_work_url = []  # Variable 
+def check_url(sample):  # Function
+    for url in sample:
+        response = requests.get(url)  # Module
+        if response.status_code == 200:  # Function
             my_work_url.append(url)  # Variable
             # my_work_url.append(url + '\n') or this to add another url in new line
         else:
             print("This page is not available:", url, response)
 
-
-def save_good_url(my_work_url):  # Method
+def save_good_url(my_work_url):  # Function
     with open("work_url.txt", "w") as file:
         file.writelines(my_work_url)
+# Save working url in new file
 
-check_url(lista_url)
+check_url(sample_file_url)
 save_good_url(my_work_url)
 
-# for url in lista_url:
-#     check_url(url)
-# with open("work_url.txt", "w") as file:
-#     file.writelines(my_work_url)
+'''
+for url in lista_url:
+    check_url(url)
+with open("work_url.txt", "w") as file:
+    file.writelines(my_work_url)
+'''
--- test_pierwszy.py
+++ test_pierwszy.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
 
+import requests
 
+sample_file_url = [
+                    'https://flask.palletsprojects.com/en/1.1.x/quickstart/', 'https://github.com/fuck',
+                    'https://www.google.pl/', 'https://www.google.pl/nocos'
+                   ]  # Sample url to test
 
+my_work_url = [] 
+def check_url(sample):
+    for url in sample:
+        response = requests.get(url)  # Check response sample url 
+        if response.status_code == 200: 
+            my_work_url.append(url)
+        else:
+            print("This page is not available:", url, response)
+
+def save_good_url(my_work_url):
+    with open("work_url.txt", "w") as file:
+        file.writelines(my_work_url)
+
+check_url(sample_file_url)
+save_good_url(my_work_url)
