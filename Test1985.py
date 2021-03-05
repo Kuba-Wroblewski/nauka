dfa2830282be252aabdc5b9ce0e6f1c490dd5312
Kuba <j.rafal.wroblewski@gmail.com>
2021-03-05 16:30:22 +0100

    update

--- Test1985.py
+++ Test1985.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
 
-import requests # import Module
+class Car():
+    def __init__(self, marka):
+        self.marka = marka
 
-sample_file_url = [
-                    'https://flask.palletsprojects.com/en/1.1.x/quickstart/', 'https://github.com/fuck',
-                    'https://www.google.pl/', 'https://www.google.pl/nocos'
-                   ]
+    def info(self):
+        print("Marka:", self.marka)
 
-my_work_url = []  # Variable 
-def check_url(sample):  # Function
-    for url in sample:
-        response = requests.get(url)  # Module
-        if response.status_code == 200:  # Function
-            my_work_url.append(url)  # Variable
-            # my_work_url.append(url + '\n') or this to add another url in new line
-        else:
-            print("This page is not available:", url, response)
 
-def save_good_url(my_work_url):  # Function
-    with open("work_url.txt", "w") as file:
-        file.writelines(my_work_url)
-# Save working url in new file
+first_car = Car("Skoda")
+second_car = Car("Opel")
 
-check_url(sample_file_url)
-save_good_url(my_work_url)
-
-'''
-for url in lista_url:
-    check_url(url)
-with open("work_url.txt", "w") as file:
-    file.writelines(my_work_url)
-'''
+first_car.info()
+second_car.info()
--- test_pierwszy.py
+++ test_pierwszy.py
@@ -2,21 +2,24 @@
 
 import requests
 
+# Sample url to test
 sample_file_url = [
                     'https://flask.palletsprojects.com/en/1.1.x/quickstart/', 'https://github.com/fuck',
                     'https://www.google.pl/', 'https://www.google.pl/nocos'
-                   ]  # Sample url to test
+                   ]  
 
 my_work_url = [] 
 def check_url(sample):
     for url in sample:
-        response = requests.get(url)  # Check response sample url 
+        # Check response sample url
+        response = requests.get(url)   
         if response.status_code == 200: 
             my_work_url.append(url)
         else:
             print("This page is not available:", url, response)
 
 def save_good_url(my_work_url):
+    
     with open("work_url.txt", "w") as file:
         file.writelines(my_work_url)
 
